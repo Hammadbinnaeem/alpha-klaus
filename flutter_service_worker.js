9564eee98b0210d89e730e87a1e9636fb0bb53a3
Hammadbinnaeem <75252249+Hammadbinnaeem@users.noreply.github.com>
2022-06-02 19:28:09 +0500

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -327,8 +327,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "94464dfad00c26586596ee7901d7d45f",
-"/": "94464dfad00c26586596ee7901d7d45f",
+"index.html": "08b8717c2ffc168bb048a6418ddc9794",
+"/": "08b8717c2ffc168bb048a6418ddc9794",
 "main.dart.js": "6ac30a37b89a90457fab099fd6babde0",
 "manifest.json": "5512dcc970ca0f3a048b127471ca230d",
 "version.json": "30c8a8b9c1eec289b6a1e17b76224efb"
